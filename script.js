240a5acd2edf3f2511b8e0d296be52d8793d1104
CRUZ1807 <henriquecc2006@gmail.com>
2023-03-09 10:12:51 -0300

    terminado por agora

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 function d4(){
     console.log ('dado 4 clicked');
-    var resultado = [(randomNumber(1, 4))]
-    console.log (resultado);
+    var resultado = Math.floor(Math.random() * 4);
+    if(resultado < 1){
+        resultado =+1;
+    }
+    console.log(resultado)
 }
 function d6(){
     console.log ('dado 6 clicked');
-    var resultado = [(randomNumber(1, 6))]
-    console.log (resultado)
+    var resultado = Math.floor(Math.random() * 6);
+    if(resultado < 1){
+        resultado =+1;
+    }
+    console.log(resultado)
+}
+function d8(){
+    console.log ('dado 8 clicked');
+    var resultado = Math.floor(Math.random() * 8);
+    if(resultado < 1){
+        resultado =+1;
+    }
+    console.log(resultado)
 }
 function d10(){
     console.log ('dado 10 clicked');
-    var resultado = [(randomNumber(1, 10))];
-    console.log (resultado);
+    var resultado = Math.floor(Math.random(1) * 10);
+    if(resultado < 1){
+        resultado =+1;
+    }
+    console.log(resultado)
 }
 function d12(){
-    console.log ('dado d12 clicked');
-    var resultado = [(randomNumber(1, 12))];
-    console.log(resultado);
+    console.log ('dado 12 clicked');
+    var resultado = Math.floor(Math.random() * 12);
+    //if(resultado < 1){
+        //resultado =+1;
+    //}
+    console.log(resultado)
 }
 function d20(){
-        console.log("dado 20 clicked");
-        var resultado = [(randomNumber(1, 20))]
-        console.log (resultado);
+    console.log ('dado 20 clicked');
+    var resultado = Math.floor(Math.random() * 20);
+    if(resultado < 1){
+        resultado =+1;
+    }
+    console.log(resultado)
 }
 function d100(){
-    console.log("dado 100 clicked");
-    var resultado = [(randomNumber(1, 100))]
-    console.log (resultado);
+    console.log ('dado 100 clicked');
+    var resultado = Math.floor(Math.random() * 100);
+    if(resultado < 1){
+        resultado =+1;
+    }
+    console.log(resultado)
 }
