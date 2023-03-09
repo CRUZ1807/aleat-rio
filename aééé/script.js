adf76e1611d0ce00bf1ecd26be2fdb6b2c30b7d1
CRUZ1807 <henriquecc2006@gmail.com>
2023-03-09 09:48:19 -0300

    algo tá dando problema

---
+++ aééé/script.js
@@ -0,0 +1,30 @@
+function d4(){
+    console.log ('dado 4 clicked');
+    var resultado = [(randomNumber(1, 4))]
+    console.log (resultado);
+}
+function d6(){
+    console.log ('dado 6 clicked');
+    var resultado = [(randomNumber(1, 6))]
+    console.log (resultado)
+}
+function d10(){
+    console.log ('dado 10 clicked');
+    var resultado = [(randomNumber(1, 10))];
+    console.log (resultado);
+}
+function d12(){
+    console.log ('dado d12 clicked');
+    var resultado = [(randomNumber(1, 12))];
+    console.log(resultado);
+}
+function d20(){
+        console.log("dado 20 clicked");
+        var resultado = [(randomNumber(1, 20))]
+        console.log (resultado);
+}
+function d100(){
+    console.log("dado 100 clicked");
+    var resultado = [(randomNumber(1, 100))]
+    console.log (resultado);
+}
