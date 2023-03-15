e66550e3b4a1cdc3ec285116c0f3dc68b1d853c1
CRUZ1807 <henriquecc2006@gmail.com>
2023-03-15 08:54:47 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-function d4(){
-    console.log ('dado 4 clicked');
-    var resultado = Math.floor(Math.random() * 4);
-    if(resultado < 1){
-        resultado =+1;
+    var div = document.getElementById('div');
+    function d4(){
+        console.log ('dado 4 clicked');
+        var resultado = Math.floor(Math.random() * 4);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
     }
-    console.log(resultado)
-}
-function d6(){
-    console.log ('dado 6 clicked');
-    var resultado = Math.floor(Math.random() * 6);
-    if(resultado < 1){
-        resultado =+1;
+    function d6(){
+        console.log ('dado 6 clicked');
+        var resultado = Math.floor(Math.random() * 6);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
     }
-    console.log(resultado)
-}
-function d8(){
-    console.log ('dado 8 clicked');
-    var resultado = Math.floor(Math.random() * 8);
-    if(resultado < 1){
-        resultado =+1;
+    function d8(){
+        console.log ('dado 8 clicked');
+        var resultado = Math.floor(Math.random() * 8);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
     }
-    console.log(resultado)
-}
-function d10(){
-    console.log ('dado 10 clicked');
-    var resultado = Math.floor(Math.random(1) * 10);
-    if(resultado < 1){
-        resultado =+1;
+    function d10(){
+        console.log ('dado 10 clicked');
+        var resultado = Math.floor(Math.random(1) * 10);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
     }
-    console.log(resultado)
-}
-function d12(){
-    console.log ('dado 12 clicked');
-    var resultado = Math.floor(Math.random() * 12);
-    //if(resultado < 1){
-        //resultado =+1;
-    //}
-    console.log(resultado)
-}
-function d20(){
-    console.log ('dado 20 clicked');
-    var resultado = Math.floor(Math.random() * 20);
-    if(resultado < 1){
-        resultado =+1;
+    function d12(){
+        console.log ('dado 12 clicked');
+        var resultado = Math.floor(Math.random() * 12);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
     }
-    console.log(resultado)
-}
-function d100(){
-    console.log ('dado 100 clicked');
-    var resultado = Math.floor(Math.random() * 100);
-    if(resultado < 1){
-        resultado =+1;
+    function d20(){
+        console.log ('dado 20 clicked');
+        var resultado = Math.floor(Math.random() * 20);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
     }
-    console.log(resultado)
-}
+    function d100(){
+        console.log ('dado 100 clicked');
+        var resultado = Math.floor(Math.random() * 100);
+        if(resultado < 1){
+            resultado =+1;
+        }
+        console.log(resultado)
+    }
+    function dados(resultado){
+        if(resultado == 1){
+            document.getElementById("")
+        }
+    }
